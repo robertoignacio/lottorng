b10e73ded65c450f9efbd42eb549923066b1dbd1
robertoignacio <rvs.git@gmail.com>
2025-01-30 17:34:33 -0300

    context header

---
+++ app/ui/components/ContextHeader.tsx
@@ -0,0 +1,16 @@
+
+// body font: noto_sans
+// accent font: montserrat
+import { montserrat } from "@/app/ui/fonts";
+
+export default function ContextHeader() {
+    return (
+        <div className="border-2 border-solid border-blue-600">
+        <p>component: ContextHeader</p>
+        <p>accesa: null</p>
+        <p>acción: display objetivo</p>
+        <h1 className={`${montserrat.className} text-xl font-bold antialiased`}>title: Lotto RNG</h1>
+        <p>descr: Generar combinación para jugarla en el lotto</p>
+        </div>
+    )
+};
