23cccb9233e18e08f2e2593e7eb864cfcef2dfe1
robertoignacio <rvs.git@gmail.com>
2025-01-30 17:22:28 -0300

    generate combinations

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 // accent font: montserrat
 import { montserrat } from "@/app/ui/fonts";
 
+import GenerateCombinations from "./ui/components/GenerateCombinations";
+
 export default function Home() {
   return (
     <>
@@ -35,16 +37,7 @@ export default function Home() {
       </div>
 
       {/* to do: react component: GenerateCombinations */}
-      <div className="border-2 border-solid border-green-600">
-        <p>component: GenerateCombinations</p>
-        <p>accesa: objeto anterior generado por NumberRange</p>
-        <p>acción: continúa chain</p>
-        <button className="border-2 border-solid border-orange-600">Botón: Generar</button>
-        <p>Total de combinaciones: este número</p>
-        <div className="border-2 border-solid border-orange-600">
-        <p>Tabla de resultados: drop down (truncado|full), pagination?</p>
-        </div>
-      </div>
+      <GenerateCombinations />
 
       {/* to do: react component: GetRandomCombination */}
       <div className="border-2 border-solid border-fuchsia-600">
---
+++ app/ui/components/GenerateCombinations.tsx
@@ -0,0 +1,15 @@
+
+export default function GenerateCombinations() {
+    return (
+        <div className="border-2 border-solid border-green-600">
+            <p>component: GenerateCombinations</p>
+            <p>accesa: objeto anterior generado por NumberRange</p>
+            <p>acción: continúa chain</p>
+            <button className="border-2 border-solid border-orange-600">Botón: Generar</button>
+            <p>Total de combinaciones: este número</p>
+            <div className="border-2 border-solid border-orange-600">
+            <p>Tabla de resultados: drop down (truncado|full), pagination?</p>
+            </div>
+        </div>
+    )
+};
