211db12868ff07551d1bd1413df5fcc6bde5787d
robertoignacio <rvs.git@gmail.com>
2025-01-31 00:03:26 -0300

    configuring number range

--- app/ui/components/NumberRange.tsx
+++ app/ui/components/NumberRange.tsx
@@ -1,17 +1,24 @@
 
+// style vars visual reference
+// const styleBorder_Temp = "";
+const styleBorder_Temp1 = "border-2 border-solid border-orange-600 my-4 py-2";
+const styleBorder_Temp2 = "border-2 border-solid border-orange-600";
+const styleText01 = "text-xs font-normal antialiased";
+const styleButtonAddSub = "border-2 border-solid border-white bg-black box-content text-2xl font-bold text-white antialiased";
+
 export default function NumberRange() {
     return (
-        <div className="border-2 border-solid border-orange-600">
-        <p>component: NumberRange</p>
-        <p>accesa: null</p>
-        <p>acción: definir rango, dato inicio chain</p>
-        <p className="text-s font-normal antialiased">descr: Definir el rango de números</p>
+        <div className={styleBorder_Temp1}>
         <div className="flex justify-around">
-          <div className="border-2 border-solid border-orange-600">
-            <p className="text-s font-normal antialiased">rango inferior</p>
+          <div className={styleBorder_Temp2}>
+            <p className={styleText01}>rango inferior</p>
+            <div className="flex flex-col">
+            <button className={styleButtonAddSub}>+</button>
+            <button className={styleButtonAddSub}>-</button>
+            </div>
           </div>
           <div className="border-2 border-solid border-orange-600">
-          <p className="text-s font-normal antialiased">rango superior</p>
+          <p className={styleText01}>rango superior</p>
           </div>          
         </div>
       </div>
