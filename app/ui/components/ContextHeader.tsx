dfc05bae6ddb45c9f4220d5e3bef05ddb6a16e0e
robertoignacio <rvs.git@gmail.com>
2025-01-30 21:45:07 -0300

    context header cleanup

--- app/ui/components/ContextHeader.tsx
+++ app/ui/components/ContextHeader.tsx
@@ -4,14 +4,14 @@
 import { montserrat } from "@/app/ui/fonts";
 
 // style vars
-const styleBorder_Temp = "border-2 border-solid border-blue-600";
+const styleBorder_Temp = "";
+//const styleBorder_Temp = "border-2 border-solid border-blue-600";
 const styleTitle = `${montserrat.className} text-xl font-bold antialiased`;
 
 export default function ContextHeader() {
     return (
         <div className={styleBorder_Temp}>
         <h1 className={styleTitle}>Generador de Números para jugar al Lotto</h1>
-        <p>Instrucciones:</p>
+        <p>Defina el rango de números</p>
         </div>
-    )
-};
+    )};
